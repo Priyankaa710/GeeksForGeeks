d7b4a64289048b16e0ca6d8648f9b8f026a10f1a
Priyanka Nagare <pnagare2004@gmail.com>
2026-07-30 22:35:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Distinct Sorted/distinct-sorted.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> uniqueSorted(int arr[]) {
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        return new ArrayList<>(set);
+    }
+}
